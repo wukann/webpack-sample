5e97ffc89cd507cd7e8c5cb768255b4ffb26d1d2
Hiroyuki Kano <wukann@gmail.com>
2017-02-04 00:54:46 +0900

    add webpack optimizer

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,12 @@
+const webpack = require('webpack');
+
 module.exports = {
   entry: './src/index.js',
   output: {
     path: 'dist',
     filename: 'bundle.js'
-  }
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin()
+  ]
 };
